50526d4eb5d000aa0caea364dbbe1a3664ca8e31
Cole Fortner <colefortner@gmail.com>
2021-06-15 17:42:17 +0000

    another test

--- candidate-testing.js
+++ candidate-testing.js
@@ -111,7 +111,7 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers = [];
-let score = 0;
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -141,6 +141,7 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
     let grade;
+    let score = 0;
     for (let i = 0; i < questions.length; i++){
         if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
       score = score + 1;
